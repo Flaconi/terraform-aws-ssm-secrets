5e2bd0330913e5502a0225718961721c388f4cde
alexander.miehe <alexander.miehe@flaconi.de>
2024-11-29 14:29:34 +0100

    PLT-0 - Move module from sharded to extra repo
    * setup module

---
+++ main.tf
@@ -0,0 +1,26 @@
+moved {
+  from = aws_ssm_parameter.this
+  to   = module.ssm.aws_ssm_parameter.this
+}
+
+module "ssm" {
+  source = "github.com/Flaconi/terraform-aws-ssm-store?ref=v1.2.0"
+
+  tags        = var.tags
+  kms_alias   = var.kms_alias
+  name_prefix = var.name_prefix
+  parameters  = var.parameters
+}
+
+module "secrets" {
+  source = "github.com/terraform-aws-modules/terraform-aws-secrets-manager?ref=v1.1.2"
+
+  tags = var.tags
+
+  kms_key_id = "alias/aws/secretsmanager"
+
+  name        = trimsuffix(var.name_prefix, "/")
+  description = "Secrets for the ${var.tags.Project} application"
+
+  secret_string = jsonencode({ for parameter in var.parameters : parameter.name => parameter.value })
+}
